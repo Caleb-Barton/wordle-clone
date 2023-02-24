defbc8f3bd08403948f050a4c492cf5babd322de
Caleb-Barton <calebbarton0@gmail.com>
2023-02-24 11:20:30 -0700

    Hey, Wordle is working!
    Neat. Now I need to make Up And Downle

--- js/main.js
+++ js/main.js
@@ -4,26 +4,58 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let guessedWords = [[]];
     let availableSpace = 1;
+    let allWords;
+    getAllWords();
 
-    let word = "thing";
+    let word;
     let guessedWordCount = 0;
 
     const keys = document.querySelectorAll(".keyboard-row button");
+
+    function getAllWords() {
+      fetch("data/all_words.txt")
+            .then(response => response.text())
+            .then(text => {
+              allWords = text.split('\r\n')
+            })
+            .catch((err) => {
+                console.error(err);
+            }
+        );
+    }
 
     function getNewWord() {
         fetch("data/good_words.txt")
             .then(response => response.text())
             .then(text => {
-                const listItems = text.split('\n').map(item => `<li>${item}</li>`).join('');
-                const list = document.createElement('ul');
-                list.innerHTML = listItems;
-                document.body.appendChild(list);
+              const good_words = text.split('\r\n');
+              let index = getNumberFromDate(good_words.length)
+              word = good_words[index];
             })
             .catch((err) => {
                 console.error(err);
                 word = "error"
             }
         );
+    }
+
+    function getNumberFromDate(length) { // Gifted to me by ChatGPT
+      const date = new Date();
+      const dateString = date.toISOString().slice(0, 10); // get the date string in "YYYY-MM-DD" format
+      const hash = getHash(dateString); // get a hash value for the date string
+
+      const number = hash % length; // map the hash value to a number between 1 and 300
+      return number;
+    }
+    
+    function getHash(string) { // Gifted to me by ChatGPT
+      let hash = 0;
+      for (let i = 0; i < string.length; i++) {
+        const charCode = string.charCodeAt(i);
+        hash = ((hash << 5) - hash) + charCode;
+        hash = hash & hash; // convert to 32-bit integer
+      }
+      return Math.abs(hash); //This kept returning a negative number, which I guess is harder to index in javascript, so....
     }
 
   function getCurrentWordArr() {
@@ -68,48 +100,59 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     const currentWord = currentWordArr.join("");
+    console.log(binarySearch(allWords, currentWord));
+    
+    if (binarySearch(allWords, currentWord)) {
+      const firstLetterId = guessedWordCount * 5 + 1;
+      const interval = 200;
+      currentWordArr.forEach((letter, index) => {
+        setTimeout(() => {
+          const tileColor = getTileColor(letter, index);
 
-    fetch(`https://wordsapiv1.p.rapidapi.com/words/${currentWord}`, {
-      method: "GET",
-      headers: {
-        "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-        "x-rapidapi-key": "61c5e3986dmsh20c1bee95c2230dp18d1efjsn4668bbcfc1b3",
-      },
-    })
-      .then((res) => {
-        if (!res.ok) {
-          throw Error();
-        }
-
-        const firstLetterId = guessedWordCount * 5 + 1;
-        const interval = 200;
-        currentWordArr.forEach((letter, index) => {
-          setTimeout(() => {
-            const tileColor = getTileColor(letter, index);
-
-            const letterId = firstLetterId + index;
-            const letterEl = document.getElementById(letterId);
-            letterEl.classList.add("animate__flipInX");
-            letterEl.style = `background-color:${tileColor};border-color:${tileColor}`;
-          }, interval * index);
-        });
-
-        guessedWordCount += 1;
-
-        if (currentWord === word) {
-          window.alert("Congratulations!");
-        }
-
-        if (guessedWords.length === 6) {
-          window.alert(`Sorry, you have no more guesses! The word is ${word}.`);
-        }
-
-        guessedWords.push([]);
-      })
-      .catch(() => {
-        window.alert("Word is not recognised!");
+          const letterId = firstLetterId + index;
+          const letterEl = document.getElementById(letterId);
+          letterEl.classList.add("animate__flipInX");
+          letterEl.style = `background-color:${tileColor};border-color:${tileColor}`;
+        }, interval * index);
       });
+
+      guessedWordCount += 1;
+
+      if (currentWord === word) {
+        window.alert("Congratulations!");
+      }
+
+      if (guessedWords.length === 6) {
+        window.alert(`Sorry, you have no more guesses! The word is ${word}.`);
+      }
+
+      guessedWords.push([]);
+
+    } else {
+      window.alert("Word is not recognised!");
+    }
   }
+
+  function binarySearch(arr, target) { // Gifted to me by ChatGPT
+    let left = 0;
+    let right = arr.length - 1;
+    
+    while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+      const comparison = target.localeCompare(arr[mid]);
+      
+      if (comparison === 0) {
+        return true; // target found
+      } else if (comparison < 0) {
+        right = mid - 1; // target is in left half of array
+      } else {
+        left = mid + 1; // target is in right half of array
+      }
+    }
+    
+    return false; // target not found
+  }
+  
 
   function createSquares() {
     const gameBoard = document.getElementById("board");
