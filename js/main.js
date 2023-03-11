39ef3896d18448f69e1e3257ea045aeb88a46292
Caleb-Barton <calebbarton0@gmail.com>
2023-03-10 17:24:47 -0700

    Some changes, I guess

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     createSquares();
     getNewWord();
+    initHelpModal();
 
     let guessedWords = [[]];
     let availableSpace = 1;
@@ -302,5 +303,28 @@ document.addEventListener("DOMContentLoaded", () => {
         typeToBoard(letter);
       }
     };
+  }
+
+  function initHelpModal() {
+    const modal = document.getElementById("help-modal");
+    // Get the button that opens the modal
+    const btn = document.getElementById("help");
+    // Get the <span> element that closes the modal
+    const span = document.getElementById("close-help");
+    // When the user clicks on the button, open the modal
+    btn.addEventListener("click", function () {
+      modal.style.display = "block";
+    });
+    // When the user clicks on <span> (x), close the modal
+    span.addEventListener("click", function () {
+      modal.style.display = "none";
+    });
+    // When the user clicks anywhere outside of the modal, close it
+    window.addEventListener("click", function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    });
+    modal.style.display = "block";
   }
 });
